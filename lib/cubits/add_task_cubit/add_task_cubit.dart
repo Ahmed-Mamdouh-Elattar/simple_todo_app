c57e878577757f55175813c719b69ea6fc2a788a
Ahmed Mamdouh <ahmed.mamdouh.elattar@gmail.com>
2024-07-23 14:28:06 +0300

    changing task cubit to add task cubit

---
+++ lib/cubits/add_task_cubit/add_task_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:simple_todo_app/constants.dart';
+import 'package:simple_todo_app/models/task_model.dart';
+
+part 'add_task_state.dart';
+
+class AddTaskCubit extends Cubit<TaskState> {
+  AddTaskCubit() : super(TaskInitial());
+
+  void addTask(TaskModel task) {
+    emit(TaskLoading());
+    try {
+      Hive.box<TaskModel>(kBoxName).add(task);
+      emit(TaskSuccess());
+    } catch (e) {
+      emit(TaskFailure(errMessage: e.toString()));
+    }
+  }
+}
